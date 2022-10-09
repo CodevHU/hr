40db59df84da3790fb134b9b5d527f05a42de991
Attila Domokos <attila@codev.hu>
2022-10-09 20:43:10 +0200

    - Add EmployeeDto - Add REST

---
+++ hr/src/main/java/hu/webuni/hr/domi/dto/EmployeeDto.java
@@ -0,0 +1,66 @@
+package hu.webuni.hr.domi.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class EmployeeDto {
+
+	private long id;
+	private String name;
+	private String workPosition;
+	private int pay;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+	private LocalDateTime firstWorkingDay;
+	
+	
+	public EmployeeDto(long id, String name, String workPosition, int pay, LocalDateTime firstWorkingDay) {
+		this.id = id;
+		this.name = name;
+		this.workPosition = workPosition;
+		this.pay = pay;
+		this.firstWorkingDay = firstWorkingDay;
+	}
+
+	public long getIdentifier() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getWorkPosition() {
+		return workPosition;
+	}
+
+	public int getPay() {
+		return pay;
+	}
+
+	public LocalDateTime getFirstWorkingDay() {
+		return firstWorkingDay;
+	}
+
+	public void setIdentifier(long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setWorkPosition(String workPosition) {
+		this.workPosition = workPosition;
+	}
+
+	public void setPay(int pay) {
+		this.pay = pay;
+	}
+
+	public void setFirstWorkingDay(LocalDateTime firstWorkingDay) {
+		this.firstWorkingDay = firstWorkingDay;
+	}
+
+}
---
+++ hr/src/main/java/hu/webuni/hr/domi/web/EmployeeController.java
@@ -0,0 +1,103 @@
+package hu.webuni.hr.domi.web;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.webuni.hr.domi.dto.EmployeeDto;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+	Map<Long, EmployeeDto> allEmployees = new HashMap<>();
+
+	{
+		allEmployees.put(1L, 
+				new EmployeeDto(1, "Kóst Elek", "CEO", 1000000, LocalDateTime.of(2010, 1, 14, 10, 34)));
+		allEmployees.put(2L,
+				new EmployeeDto(2, "Lapos Elemér", "Coordinator", 200000, LocalDateTime.of(2014, 1, 14, 10, 34)));
+		allEmployees.put(3L,
+				new EmployeeDto(3, "Lusta Gyula", "Manager", 300000, LocalDateTime.of(2018, 1, 14, 10, 34)));
+		allEmployees.put(4L,
+				new EmployeeDto(4, "Kiss Zoltán", "Project Manager", 800000, LocalDateTime.of(2020, 1, 14, 10, 34)));
+	}
+
+	@GetMapping
+	public List<EmployeeDto> getAll() {
+		return new ArrayList<>(allEmployees.values());
+	}
+	
+	@GetMapping("/filter")
+	public List<EmployeeDto> getFilterListByPay(@RequestParam(name = "pay") int pay) {
+		
+		Map<Long, EmployeeDto> filteredEmployees = allEmployees
+				.entrySet()
+				.stream()
+				.filter(employee -> employee.getValue().getPay() > pay)
+				.collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+		
+		return new ArrayList<>(filteredEmployees.values());
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<EmployeeDto> getById(@PathVariable long id) {
+		
+		if(!allEmployees.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(allEmployees.get(id));
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDto> addEmployee(@RequestBody EmployeeDto employee){
+		allEmployees.put(employee.getIdentifier(), employee);
+		
+		return ResponseEntity.ok(employee);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable long id, @RequestBody EmployeeDto employee){
+		
+		if(!allEmployees.containsKey(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		employee.setIdentifier(id);
+		allEmployees.put(id,employee);
+		
+		return ResponseEntity.ok(allEmployees.get(id));
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public boolean deleteEmployee(@PathVariable long id){
+		
+		if(!allEmployees.containsKey(id)) {
+			return false;
+		}
+		
+		allEmployees.remove(id);
+		
+		return true;
+		
+	}
+
+}
